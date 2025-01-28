a0c2aad71042fa993683b5c3817c23ad756b15d1
jvrdoljak <vrdoljakjosip@outlook.com>
2025-01-28 19:28:38 +0100

    feat: add sorting

--- src/superheroes/superheroes.service.ts
+++ src/superheroes/superheroes.service.ts
@@ -12,11 +12,13 @@ const dataFilePath = './superheroes_data/superheroes.json';
 @Injectable()
 export class SuperheroesService {
   findAllSuperheroes(): Array<Superhero> {
-    const file = readFileSync(dataFilePath, 'utf-8');
-    if (!file) return [];
-    const superheroes: Array<Superhero> = JSON.parse(file) as Array<Superhero>;
+    const fileData = readFileSync(dataFilePath, 'utf-8');
+    if (!fileData) return [];
+    const superheroes: Array<Superhero> = JSON.parse(fileData) as Array<Superhero>;
 
-    return superheroes;
+    return superheroes.sort((a, b) => {
+      return b.humilityScore - a.humilityScore;
+    });
   }
 
   addSuperhero(superhero: Superhero): boolean {
